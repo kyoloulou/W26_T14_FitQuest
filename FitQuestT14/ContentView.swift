caa27ab4e10d9b248403e912d909d41398db9381
trunganh <trunganh.ngptv@gmail.com>
2026-03-25 16:46:01 -0700

    Initial Commit

---
+++ FitQuestT14/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitQuestT14
+//
+//  Created by user291485 on 3/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitQuestT14/FitQuestT14App.swift
@@ -0,0 +1,17 @@
+//
+//  FitQuestT14App.swift
+//  FitQuestT14
+//
+//  Created by user291485 on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FitQuestT14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
